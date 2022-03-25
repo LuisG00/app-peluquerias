44e052428e6cae17e39e72689b7b653498187bed
Madeline-Esc <made_2500@hotmail.com>
2022-03-24 22:26:47 -0600

    screens Madeline

---
+++ app-peluquerias/app/screens/Barberias.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Barberias(){
+    return(
+        <View>
+            <Text>Barberías</Text>
+        </View>
+    )
+}
